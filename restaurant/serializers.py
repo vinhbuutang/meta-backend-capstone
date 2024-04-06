f7e3b647776563f33b3900cf0d82524851c727d1
vinhbuutang <buuvinhpro@gmail.com>
2024-04-06 00:29:15 -0400

    set up booking API

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from restaurant.models import Menu
+from restaurant.models import Menu, Booking
 
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
+        fields = "__all__"
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
         fields = "__all__"
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import Menu
-from .serializers import MenuSerializer
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 
 def index(request):
@@ -16,3 +16,8 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
